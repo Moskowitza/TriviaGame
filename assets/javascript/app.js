1e38d1a994c46e6cd90adcfc6613c72a7496149d
Aaron Moskowitz <macPRO@Aarons-MacBook-Pro-2.local>
2018-03-02 12:18:58 -0500

    looks good, but needs to deterime answers and move questions to the script, would love to store as objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,3 +6,79 @@
 //next screen, show correct answers, incorrect answers, AND not clicked
 
 //setTimeout(function, milliseconds)
+
+$(document).ready(function () {
+    $("#quiz").hide();
+    $("#results").hide();
+
+
+    //  Interval Demonstration
+    //  Set our number counter to 100.
+    var number = 100;
+
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+    var intervalId;
+
+     function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    //  The decrement function.
+    function decrement() {
+      number--;
+
+      $("#show-number").html("<h2>" + number + "</h2>");
+
+
+      //  Once number hits zero...
+      if (number === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+        alert("Time Up!");
+      }
+    }
+
+    //  The stop function
+    function stop() {
+
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+    }
+
+    //  Execute the run function.
+    run();
+
+    function timer() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+      }
+  
+    //On Start
+    $("#startBtn").click(function () {
+        $("#start").hide();
+        $("#quiz").show();
+        //start timer right after 
+        setTimeout(function quiz() {
+            //quiz function
+            $("#done").click(function () {
+                $("#quiz").hide();
+                $("#results").show();
+            });
+
+        }, 1000 + 5);
+    });
+
+
+    $("#return").click(function () {
+        $("#results").hide();
+        $("#start").show();
+    });
+
+});
