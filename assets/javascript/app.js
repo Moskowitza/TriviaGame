92ba737d26cb5ebaacd0332a02c91c3dd4522cd3
Aaron Moskowitz <macPRO@Aarons-MacBook-Pro-2.local>
2018-03-02 07:58:54 -0500

    index is ready to play with

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// starts with a button click "start"
+//timer starts automatically demo has 30s, displayed at the top
+//quiz is multiple choice
+//ends when the timer hits zero
+//there is a DONE button
+//next screen, show correct answers, incorrect answers, AND not clicked
+
+//setTimeout(function, milliseconds)
